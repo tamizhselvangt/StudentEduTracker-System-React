35e4078476ec00c8aa5b21c786ddb0dd60f27cfa
tamizhselvangt <tamizhselvangt@gmail.com>
2024-08-29 20:19:32 +0530

    first commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,10 +3,12 @@ import './Card.css'
 
 export default function Card({description, icon , initialVal , color}){
   return (
-  <div className="card" style={{ backgroundColor: {color}}}>
+  <div className="card" style={{ 
+    backgroundColor: color
+  }}>
     <div className="card-top">
-   <span className='icon'>{icon}</span>
-  {initialVal}
+       <span className='icon'>{icon}</span>
+      {initialVal}
     </div>
    <div className='card-bottom'>{description}</div>
   </div>)
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,7 +1,16 @@
 
 import Card from './Card'
 import './Main.css'
+import './SiderBar.css'
 import { IoPerson } from "react-icons/io5";
+import { FaUserTie } from "react-icons/fa";
+import { FaStreetView } from "react-icons/fa6";
+import { FaBook } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaBookReader } from "react-icons/fa";
+import { BiSolidReport } from "react-icons/bi";
+import { FaRegHospital } from "react-icons/fa";
+import { IoPersonSharp } from "react-icons/io5";
 export default function Main(){
   return(
   <div className="main">
@@ -10,10 +19,34 @@ export default function Main(){
     </div>
     <div className='middle-bar'>
       <Card description={"Regular Studnets"} icon={ <IoPerson  />} initialVal={8} color="red" />
-      <Card description={"Regular Studnets"} icon={ <IoPerson  />} initialVal={11}  colo="purple"/>
-      <Card description={"Regular Studnets"} icon={ <IoPerson  />} initialVal={9} color="green"/>
+      <Card description={"Regular Studnets"} icon={ <FaUserTie />} initialVal={11}  color="purple"/>
+      <Card description={"Regular Studnets"} icon={ <FaStreetView />} initialVal={9} color="green"/>
     </div>
+    <div className='menu'>
+     <MenuItem icon={<IoPersonSharp />} description={"Regular Enrolment"}/>
+     <MenuItem icon={<FaBook />} description={"Regular Enrolment"}/>
+     <MenuItem icon={<FaStreetView />} description={"Regular Enrolment"}/>
+     <MenuItem icon={<FaBookReader />} description={"Regular Enrolment"}/>
+     <MenuItem icon={<MdEmail />} description={"Regular Enrolment"}/>
+     <MenuItem icon={<BiSolidReport />} description={"Regular Enrolment"}/>
+     <MenuItem icon={<FaRegHospital />} description={"Regular Enrolment"}/>
+    </div>
+    <div></div>
   </div>)
 }
 
 
+
+function MenuItem({icon, description}){
+ return(
+  <div className="menu-bar-grp">
+  <div className='menu-bar-item'>
+{icon}
+  </div>
+  <div className='menu-bar-lable'>
+{description}
+  </div>
+</div>
+ )
+
+}
